f02dc37b1e0da04bfde93e5503ebf599115dfb25
Aboubacar Zoungrana <aboubacarprofessional@yahoo.com>
2023-05-08 14:45:43 -0500

    js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var button = document.querySelector('button');
 var footer = document.getElementById('header');
 
-button.onclick = function changeColor(){
-    footer.style.background = 'lightgreen'
-};
+// button.onclick = function changeColor(){
+//     footer.style.background = 'lightgreen'
+// };  
+function changeColor(){
+    if(footer.style.background == 'lightgreen'){
+        footer.style.background = 'blue';
+    }else{
+        footer.style.background = 'lightgreen';
+    }
+}
