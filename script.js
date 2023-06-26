cbcdf169e48f04bf2afd6ed5120e2618c328e3eb
Aboubacar Zoungrana <aboubacarprofessional@yahoo.com>
2023-06-25 22:30:03 -0500

    js

--- script.js
+++ script.js
@@ -39,3 +39,27 @@ function changeColor(){
         footer.style.background = 'lightgreen';
     }
 }
+
+function changeColor(){
+    if(footer.style.background == 'lightgreen'){
+        footer.style.background = 'blue';
+    }else{
+        footer.style.background = 'lightgreen';
+    }
+}
+
+function changeColor(){
+    if(footer.style.background == 'lightgreen'){
+        footer.style.background = 'blue';
+    }else{
+        footer.style.background = 'lightgreen';
+    }
+}
+
+function changeColor(){
+    if(footer.style.background == 'lightgreen'){
+        footer.style.background = 'blue';
+    }else{
+        footer.style.background = 'lightgreen';
+    }
+}
